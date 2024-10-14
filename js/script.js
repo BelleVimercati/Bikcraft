716f1e35b7d15f98c7a9cc64a95e2d4486623ef1
Isabelle Vimercati <103614557+BelleVimercati@users.noreply.github.com>
2024-10-14 20:39:19 -0300

    feat: funcionalidade do FAQ

--- js/script.js
+++ js/script.js
@@ -13,6 +13,7 @@ function ativarLink(link) {
 
 links.forEach(ativarLink);
 
+
 /* Ativar itens do Orçamento */
 const parametros = new URLSearchParams(location.search);
 
@@ -24,3 +25,24 @@ function ativarProduto(parametro) {
 }
 
 parametros.forEach(ativarProduto);
+
+
+
+/* FAQ */
+const perguntas = document.querySelectorAll(".perguntas button");
+
+function ativarPergunta(event){
+    const pergunta = event.currentTarget;
+    const controls = pergunta.getAttribute("aria-controls");
+    const resposta = document.getElementById(controls)
+
+    resposta.classList.toggle("ativa");
+    const ativa = resposta.classList.contains("ativa");
+    pergunta.setAttribute("aria-expanded", ativa);
+}
+
+function eventosPerguntas(pergunta){
+    pergunta.addEventListener("click", ativarPergunta);
+}
+
+perguntas.forEach(eventosPerguntas); 
